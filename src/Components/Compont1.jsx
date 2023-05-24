78985eb0a6a02055afc59dcf690631ce5e618b1b
ATR55 <129383573+ATR55@users.noreply.github.com>
2023-05-25 02:50:29 +0300

    Update

--- src/Components/Compont1.jsx
+++ src/Components/Compont1.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import "../Style/Compont1.css";
 
 function Compont1() {
-  return <div>Compont1</div>;
+  return (
+    <div className="object-general">
+      <div className="object1">
+        <div className="object1-content">
+          <p>Plante interior</p>
+        </div>
+      </div>
+      <div className="object1">
+        <div className="object1-content">
+          <p>Plante interior</p>
+        </div>
+      </div>
+      <div className="object1">
+        <div className="object1-content">
+          <p>Plante exerior</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Compont1;
